c9efa83b69377251e58721ca32999c9abdcb7fc0
John <joaogabrielbdm@gmail.com>
2022-12-19 03:53:08 -0300

    Mais threads primeiro

--- datascript.sh
+++ datascript.sh
@@ -3,7 +3,7 @@ make time
 mkdir -p data
 for ((i = 5; i <= 8; i++)) # tamanho dos tabuleiros n x n
 do
-    for ((j = 1; j <= 16; j*=2)) # num de threads
+    for ((j = 16; j >= 1; j/=2)) # num de threads
     do
         FILE="thread${j}_board${i}.txt"
         echo "Preenchendo data/${FILE}..."
