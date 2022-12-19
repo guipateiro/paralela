6f5ab3cd33adbe5b1b039fc8eb0cb9bded502e14
GUILHERME COSTA PATEIRO <gcp19@inf.ufpr.br>
2022-12-19 17:09:48 -0300

    dados

--- datascript.sh
+++ datascript.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 make time
 mkdir -p data
-for ((i = 5; i <= 8; i++)) # tamanho dos tabuleiros n x n
+for ((i = 7; i <= 7; i++)) # tamanho dos tabuleiros n x n
 do
-    for ((j = 16; j >= 1; j/=2)) # num de threads
+    for ((j = 32; j >= 1; j/=2)) # num de threads
     do
-        FILE="thread${j}_board${i}.txt"
+        FILE="thread${j}_board${i}-00.txt"
         echo "Preenchendo data/${FILE}..."
         > data/${FILE} # apaga o conteudo dos arquivos
-        for ((NUMTIMES = 1; NUMTIMES <= 10; NUMTIMES++)) # num de execucoes do programa
+        for ((NUMTIMES = 1; NUMTIMES <= 20; NUMTIMES++)) # num de execucoes do programa
         do
-            echo $i $i $[ $RANDOM % $i] $[ $RANDOM % $i] | ./teste $j >> data/${FILE}
+            echo $i $i $[0] $[0] | ./teste $j >> data/${FILE}
         done
     done
+
+    for ((j = 32; j >= 2; j/=2)) # num de threads
+    do
+        FILE="thread${j}_board${i}-ndiv2.txt"
+        echo "Preenchendo data/${FILE}..."
+        > data/${FILE} # apaga o conteudo dos arquivos
+        for ((NUMTIMES = 1; NUMTIMES <= 20; NUMTIMES++)) # num de execucoes do programa
+        do
+            echo $i $i $[$i/2] $[$i/2] | ./teste $j >> data/${FILE}
+        done
+    done
+    
 done
--- makefile
+++ makefile
@@ -3,6 +3,9 @@ CFLAGS = -fopenmp -Wall -g -B passeio.c
 all:    passeio.c
 	gcc passeio.c -o teste $(CFLAGS)
 
+media:
+	g++ -o media media.cpp
+
 time: CFLAGS += -DTIMEONLY
 time: all
 
