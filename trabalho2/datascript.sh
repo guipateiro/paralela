9144903edb30b59c2f0b237bd3a8518e4d2dc5a7
GUILHERME COSTA PATEIRO <gcp19@inf.ufpr.br>
2023-02-07 17:09:06 -0300

    mais teste

--- trabalho2/datascript.sh
+++ trabalho2/datascript.sh
@@ -3,7 +3,7 @@ make entrada
 make
 mkdir -p data
 echo 1048576 | ./entrada > input.in
-for ((i = 16384; i <= 1048576 ; i*=2)) # tamanho da entrada
+for ((i = 32768; i <= 1048576 ; i*=2)) # tamanho da entrada
 do
     #for ((j = 32; j >= 1; j/=2)) # num de threads
     #do
@@ -18,12 +18,12 @@ do
 
     for ((j = 32; j >= 1; j/=2)) # num de threads
     do
-        FILE="thread${j}_board${i}-2048k.txt"
+        FILE="thread${j}_board${i}-32k.txt"
         echo "Preenchendo data/${FILE}..."
         > data/${FILE} # apaga o conteudo dos arquivos
         for ((NUMTIMES = 1; NUMTIMES <= 20; NUMTIMES++)) # num de execucoes do programa
         do
-            mpirun -np $j ./base input.in $i 2048 >> data/${FILE}
+            mpirun -np $j ./base input.in $i 32768 >> data/32k/${FILE}
         done
     done
     
