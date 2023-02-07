4fbcc4ca3a5ed875d117222dd2823af8327fb48e
GUILHERME COSTA PATEIRO <gcp19@inf.ufpr.br>
2023-02-07 15:06:32 -0300

    estatistca feita

--- trabalho2/estatistica.sh
+++ trabalho2/estatistica.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 make media
 mkdir -p estatistica
-> estatistica/tabela00.txt 
 for ((j = 32; j >= 1; j/=2)) # num de threads
 do
-    FILEOUT="thread${j}-00.txt"
+    FILEOUT="thread${j}-2048.txt"
     > estatistica/${FILEOUT} # apaga o conteudo dos arquivos
-    for ((i = 5; i <= 7; i++)) # tamanho dos tabuleiros n x n
+    echo "${j}" >> estatistica/${FILEOUT}
+    for ((i = 16384; i <= 1048576 ; i*=2)) # tamanho dos tabuleiros n x n
     do
-        FILEIN="thread${j}_board${i}-00.txt"
-        ./media < data/${FILEIN} >> estatistica/${FILEOUT} 
+        FILEIN="thread${j}_board${i}-2048k.txt"
+        ./media < data/2048/${FILEIN} >> estatistica/${FILEOUT} 
     done
     
 done
 cd estatistica
-paste -d ";" thread1-00.txt thread2-00.txt thread4-00.txt thread8-00.txt thread16-00.txt thread32-00.txt > tabela00.csv
-> tabelaN2.txt 
+paste -d ";" ../data/header.txt thread1-2048.txt thread2-2048.txt thread4-2048.txt thread8-2048.txt thread16-2048.txt thread32-2048.txt > tabela2048.csv
 cd ..
 
-for ((j = 32; j >= 1; j/=2)) # num de threads
-do
-    FILEOUT="thread${j}-N2.txt"
-    > estatistica/${FILEOUT} # apaga o conteudo dos arquivos
-    for ((i = 5; i <= 7; i++)) # tamanho dos tabuleiros n x n
-    do
-        FILEIN="thread${j}_board${i}-ndiv2.txt"
-        ./media < data/${FILEIN} >> estatistica/${FILEOUT}
-    done
-    
-done
+#for ((j = 32; j >= 1; j/=2)) # num de threads
+#do
+#    FILEOUT="thread${j}-16k.txt"
+#    > estatistica/${FILEOUT} # apaga o conteudo dos arquivos
+#    echo "${j}" >> estatistica/${FILEOUT}
+#    for ((i = 16384; i <= 1048576 ; i*=2)) # tamanho dos tabuleiros n x n
+#    do
+#        FILEIN="thread${j}_board${i}-16k.txt"
+#        ./media < data/${FILEIN} >> estatistica/${FILEOUT}
+#    done
+#    
+#done
 cd estatistica
-paste -d ";" thread1-N2.txt thread2-N2.txt thread4-N2.txt thread8-N2.txt thread16-N2.txt thread32-N2.txt > tabelaN2.csv
+#paste -d ";" ../data/header.txt thread1-16k.txt thread2-16k.txt thread4-16k.txt thread8-16k.txt thread16-16k.txt thread32-16k.txt > tabela16k.csv
 rm -rf *.txt
 cd .. 
 
