f75884eae353b2bea1cec17d041fee4a7e617e6b
guipateiro <guilhermepateiro@gmail.com>
2023-02-07 22:46:12 -0300

    teste o3

--- trabalho2/datascript.sh
+++ trabalho2/datascript.sh
@@ -3,28 +3,55 @@ make entrada
 make
 mkdir -p data
 echo 1048576 | ./entrada > input.in
-for ((i = 1048576; i <= 1048576 ; i*=2)) # tamanho da entrada
+for ((i = 16384; i <= 1048576 ; i*=2)) # tamanho da entrada
 do
-    #for ((j = 32; j >= 1; j/=2)) # num de threads
-    #do
-    #    FILE="thread${j}_board${i}-128.txt"
-    #    echo "Preenchendo data/${FILE}..."
-    #    > data/${FILE} # apaga o conteudo dos arquivos
-    #    for ((NUMTIMES = 1; NUMTIMES <= 20; NUMTIMES++)) # num de execucoes do programa
-    #    do
-    #       mpirun -np $j ./base input.in $i 128 >> data/${FILE}
-    #    done
-    #done
+    for ((j = 32; j >= 1; j/=2)) # num de threads
+    do
+        FILE="thread${j}_board${i}-128.txt"
+        echo "Preenchendo data/${FILE}..."
+        > data/128/${FILE} # apaga o conteudo dos arquivos
+        for ((NUMTIMES = 1; NUMTIMES <= 20; NUMTIMES++)) # num de execucoes do programa
+        do
+           mpirun -np $j ./base input.in $i 128 >> data/128/${FILE}
+        done
+    done
 
-    for ((j = 8; j >= 1; j/=2)) # num de threads
+    for ((j = 32; j >= 1; j/=2)) # num de threads
+    do
+        FILE="thread${j}_board${i}-16K.txt"
+        echo "Preenchendo data/${FILE}..."
+        > data/16K/${FILE} # apaga o conteudo dos arquivos
+        for ((NUMTIMES = 1; NUMTIMES <= 20; NUMTIMES++)) # num de execucoes do programa
+        do
+            mpirun -np $j ./base input.in $i 16374 >> data/16K/${FILE}
+        done
+    done
+
+	for ((j = 32; j >= 1; j/=2)) # num de threads
+    do
+        FILE="thread${j}_board${i}-2048.txt"
+        echo "Preenchendo data/${FILE}..."
+        > data/2048/${FILE} # apaga o conteudo dos arquivos
+        for ((NUMTIMES = 1; NUMTIMES <= 20; NUMTIMES++)) # num de execucoes do programa
+        do
+            mpirun -np $j ./base input.in $i 2048 >> data/2048/${FILE}
+        done
+    done
+    
+done
+
+for ((i = 32768; i <= 1048576 ; i*=2)) # tamanho da entrada
+do
+	 for ((j = 32; j >= 1; j/=2)) # num de threads
     do
         FILE="thread${j}_board${i}-32k.txt"
         echo "Preenchendo data/${FILE}..."
-        > data/${FILE} # apaga o conteudo dos arquivos
+        > data/32k/${FILE} # apaga o conteudo dos arquivos
         for ((NUMTIMES = 1; NUMTIMES <= 20; NUMTIMES++)) # num de execucoes do programa
         do
             mpirun -np $j ./base input.in $i 32768 >> data/32k/${FILE}
         done
     done
-    
+
+
 done
--- trabalho2/estatistica.sh
+++ trabalho2/estatistica.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 make media
 mkdir -p estatistica
-for ((j = 32; j >= 1; j/=2)) # num de threads
-do
-    FILEOUT="thread${j}-2048.txt"
-    > estatistica/${FILEOUT} # apaga o conteudo dos arquivos
-    echo "${j}" >> estatistica/${FILEOUT}
-    for ((i = 16384; i <= 1048576 ; i*=2)) # tamanho dos tabuleiros n x n
-    do
-        FILEIN="thread${j}_board${i}-2048k.txt"
-        ./media < data/2048/${FILEIN} >> estatistica/${FILEOUT} 
-    done
-    
-done
-cd estatistica
-paste -d ";" ../data/header.txt thread1-2048.txt thread2-2048.txt thread4-2048.txt thread8-2048.txt thread16-2048.txt thread32-2048.txt > tabela2048.csv
-cd ..
 
+for k in 128 2048 16k 32k 
+do
+	for ((j = 32; j >= 1; j/=2)) # num de threads
+	do
+		FILEOUT="thread${j}-${k}.txt"
+		> estatistica/${FILEOUT} # apaga o conteudo dos arquivos
+		echo "${j}" >> estatistica/${FILEOUT}
+		for ((i = 16384; i <= 1048576 ; i*=2)) # tamanho dos tabuleiros n x n
+		do
+			FILEIN="thread${j}_board${i}-${k}.txt"
+			./media < data/${k}/${FILEIN} >> estatistica/${FILEOUT} 
+		done
+		
+	done
+	cd estatistica
+	paste -d ";" ../data/header.txt thread1-${k}.txt thread2-${k}.txt thread4-${k}.txt thread8-${k}.txt thread16-${k}.txt thread32-${k}.txt > tabela-${k}.csv
+	cd ..
+done
 #for ((j = 32; j >= 1; j/=2)) # num de threads
 #do
 #    FILEOUT="thread${j}-16k.txt"
